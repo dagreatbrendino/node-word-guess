d749241a8d81f4f7682e4492241c1d823f365233
Brendan K <brendanmkelly94@gmail.com>
2019-02-19 14:51:17 -0600

    added ability to play again after guesses = 0 or word is completed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var pickWord = function(){
     var index = Math.round(Math.random() * (words.length - 1));
     current = new Word(words[index]);
     current.buildWord(0);
+    console.log(current.revealedWord(0));
 }
 
 var getGuess = function(revealed){
@@ -27,14 +28,17 @@ var getGuess = function(revealed){
             console.log("Incorrect guess!\n" + guesses + " guesses remaining");
         }
         console.log(current.revealedWord(0));
+        //if there are no more guesses remaining
         if (guesses === 0){
             console.log("You ran out of guesses! The word was " + current.word);
             gameOver()
         }
+        //if there are no more letters to reveal
         else if (displayWord.indexOf("_") === -1){
             console.log("You got the word!");
             gameOver()
         }
+        //allow for another guess
         else{
             getGuess(current.revealedWord(0));
         }        
@@ -42,6 +46,19 @@ var getGuess = function(revealed){
     
 }
 var gameOver = function(){
+    inquirer.prompt([
+        {
+        type: "confirm",
+        name: "play",
+        message: "Do you want to play again? "
+        }
+    ]).then(function(ans){
+        if (ans.play === true){
+            guesses = 8;
+            pickWord();
+            getGuess(current.revealedWord(0));
+        }
+    })
 
 }
 pickWord();
